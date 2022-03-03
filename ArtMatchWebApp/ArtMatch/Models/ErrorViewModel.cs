873b53b3e3c27cc0e57dbf5d0ae84bb118022def
jbordeau19 <jbordeau@umich.edu>
2022-03-03 17:12:46 -0500

    ASP.NET MVC

---
+++ ArtMatchWebApp/ArtMatch/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ArtMatch.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
